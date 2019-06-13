84d94f99d2d395bc4d1ae135b5b1138ab832981f
Simon Barer <simonbarer@gmail.com>
2019-06-13 16:04:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-const app = express();
+// const app = express();
 
 const { Pool } = require('pg');
 var pool = new Pool({
