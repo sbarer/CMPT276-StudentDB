30f4e1dd89c9b18698a2aa08f3e202208c5d3097
Simon Barer <simonbarer@gmail.com>
2019-06-14 13:15:50 -0700

    insert function update

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ var pool = new Pool({
 
 pool.connect();
 
+// const router = express.Router();
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -21,6 +23,15 @@ express()
       const client = await pool.connect()
       const result = await client.query('SELECT * FROM student');
       const results = { 'results': (result) ? result.rows : null};
+
+      const insertQuert = 'INSERT INTO items(name,weight,height,hair_color,gpa,major,age) values($1, $2,$3,$4,$5,$6,$7)';
+      app.post('/contact', function(req, res){
+        pool.query(insertQuery)
+      })
+      // const text = 'INSERT INTO items(name,weight,height,hair_color,gpa,major,age) values($1, $2,$3,$4,$5,$6,$7)';
+      // const data = {text: req.body.text};
+      // var values = [data.name, data.weight, data.height, data.hair_color, data.gpa, data.major, data.age];
+
       res.render('pages/db', results );
       client.release();
     } catch (err) {
@@ -28,4 +39,9 @@ express()
       res.send("Error " + err);
     }
   })
+
+  
+
+
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
