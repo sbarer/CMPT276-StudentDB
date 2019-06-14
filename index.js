a68ca0ded7d3a7c2e605a47a0c61f7519ac3e64a
Simon Barer <simonbarer@gmail.com>
2019-06-14 16:32:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ pool.connect();
 // const router = express.Router();
 
 
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
