324c04a5ad8ed88f73c320386ee2f165bbd022c2
Simon Barer <simonbarer@gmail.com>
2019-06-14 19:04:16 -0700

    bug fixes

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.post('/updateStudent', function(req,res){
   var major = req.body.major;
   var age = req.body.age;
   pool.query("UPDATE student SET name = '" + name + "' where id = '" + req.body.id + "';"); 
-  if(checknum(val)) {
+  if(checknum(weight)) {
     pool.query("UPDATE student SET weight = '" + weight + "' where id = '" + req.body.id + "';"); }
   pool.query("UPDATE student SET height = '" + height + "' where id = '" + req.body.id + "';"); 
   pool.query("UPDATE student SET hair_color = '" + hair + "' where id = '" + req.body.id + "';"); 
